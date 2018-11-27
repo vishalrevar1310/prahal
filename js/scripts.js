e2737352698b523288404ceb8615200f7d4d76fd
Tirth Gajjar <11blackspiders@gmail.com>
2018-11-27 07:35:51 +0530

    rEMOVED

--- js/scripts.js
+++ js/scripts.js
@@ -515,31 +515,31 @@
   /*  Contact Form
   /* ---------------------------------------------------------------------- */
 
-  var submitContact = $('#submit-message'),
-    message = $('#msg');
+  // var submitContact = $('#submit-message'),
+  //   message = $('#msg');
 
-  submitContact.on('click', function(e){
-    e.preventDefault();
+  // submitContact.on('click', function(e){
+  //   e.preventDefault();
 
-    var $this = $(this);
+  //   var $this = $(this);
     
-    $.ajax({
-      type: "POST",
-      url: 'contact.php',
-      dataType: 'json',
-      cache: false,
-      data: $('#gform').serialize(),
-      success: function(data) {
+  //   $.ajax({
+  //     type: "POST",
+  //     url: 'contact.php',
+  //     dataType: 'json',
+  //     cache: false,
+  //     data: $('#gform').serialize(),
+  //     success: function(data) {
 
-        if(data.info !== 'error'){
-          $this.parents('form').find('input[type=text],input[type=email],textarea,select').filter(':visible').val('');
-          message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-        } else {
-          message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-        }
-      }
-    });
-  });
+  //       if(data.info !== 'error'){
+  //         $this.parents('form').find('input[type=text],input[type=email],textarea,select').filter(':visible').val('');
+  //         message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+  //       } else {
+  //         message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+  //       }
+  //     }
+  //   });
+  // });
 
 
   /* Scroll to Top
